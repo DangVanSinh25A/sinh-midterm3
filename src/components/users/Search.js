48882bb4f167fa9596234a599977c0868b6d6383
sinh <sinhdangvan27@gmail.com>
2024-06-05 11:30:22 +0700

    api

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,34 +1,52 @@
-// Search.js
-import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Users from "./Users";
+import {searchUsers} from "../../apis/api.js"
+
 const Search = () => {
-  const [text, setText] = useState("");
-  const [users, setUsers] = useState([]);
-  const searchUsers = async (text) => {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/search/users?q=${text} `
-      );
-      setUsers(response.data.items);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-  const clearUsers = () => {
-    setUsers([]);
+    const [text, setText] = useState("");
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => { 
+        const search = localStorage.getItem("search");
+        const text = localStorage.getItem("text");
+        if (search && text) {
+            setUsers(JSON.parse(search));
+            setText(text);
+        }
+    },[]);
+
+    const search = async () => {
+        try {
+            const response = await searchUsers(text);
+            setUsers(response.items);
+            localStorage.setItem("search", JSON.stringify(response.items));
+            } 
+        catch (error) {
+            console.error("Error fetching data:", error);
+        }
     };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (text === "") {
-      alert("Please enter something");
-    } else {
-      searchUsers(text);
-      setText("");
-    }
-  };
-  const onChange = (e) => setText(e.target.value);
-  return (
+
+    const onSubmit = (e) => {
+            e.preventDefault();
+        if (text === "") {
+            alert("Please enter something");
+        } 
+        else {
+            search();
+            localStorage.setItem("text", text);
+            setText("");
+        }
+    };
+    const clearUsers = () => {
+        setUsers([]);
+        localStorage.removeItem("search");
+        localStorage.removeItem("text");
+        setText("");
+    };
+
+    const onChange = (e) => setText(e.target.value);
+
+return (
     <div>
       <form onSubmit={onSubmit} className="form">
         <input
@@ -45,12 +63,10 @@ const Search = () => {
         />
       </form>
       {users.length > 0 && (
-    <button className="btn btn-danger btn-block" onClick={clearUsers}>
-            Clear
-    </button>
-      )}
+            <button className="btn btn-danger btn-block" onClick={clearUsers}>Clear</button>
+        )}
       <Users users={users} />
     </div>
-  );
+    );
 };
 export default Search;
