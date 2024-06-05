31e803d8de16b272bc25142053b9b49c74aa3d3e
sinh <sinhdangvan27@gmail.com>
2024-06-05 10:45:46 +0700

    midterm3

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Home from "./components/layout/Home";
+import Navbar from "./components/layout/Navbar";
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
+    </div>
+  );
+};
+export default App;
